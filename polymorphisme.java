f015b364f7a319a0109de715266c8d933b7df757
denirinaldi8 <denirinaldi13@gmail.com>
2017-04-10 13:29:13 +0700

    Add files via upload

---
+++ polymorphisme.java
@@ -0,0 +1,8 @@
+public class polymorphisme extends Data {
+		public static void main(String[] args) {
+			Data data = new Data() ;
+			data.contoh("Deni Rinaldi");
+			data.contoh("590");
+			data.contoh("10.029");
+			}
+}
